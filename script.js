775e70c3d0630f1dc244b4dad2d164523a289163
pande <pandeyhariom71@gmail.com>
2024-02-10 12:43:13 +0530

    JavaScript Display Possibilities

---
+++ script.js
@@ -0,0 +1,14 @@
+function inner(){
+    document.getElementById('box').innerHTML = "Inner Text Print";
+}
+function output(){
+    document.write(7+11);
+}
+function alerts(){
+    alert("How are You??");
+}
+function consoles(){
+    console.log("I am Good");
+    console.warn("Be Careful");
+    console.error("Error Occurred");
+}
